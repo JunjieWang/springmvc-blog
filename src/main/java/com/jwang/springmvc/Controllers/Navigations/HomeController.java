ba245016d5c153d0df996b39ab0a5ad7c25b5ceb
Junjie Wang <junjiedev@gmail.com>
2016-02-21 15:44:57 -0800

    tested file upload -.-

---
+++ src/main/java/com/jwang/springmvc/Controllers/Navigations/HomeController.java
@@ -0,0 +1,19 @@
+package com.jwang.springmvc.Controllers.Navigations;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * This is the controller for the home page.
+ * Created by junjie on 2/21/16.
+ */
+@Controller public class HomeController {
+    @RequestMapping(value = {"/", "/home"}) public String greeting(
+        @RequestParam(value = "name", required = false, defaultValue = "World") String name,
+        Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+}
